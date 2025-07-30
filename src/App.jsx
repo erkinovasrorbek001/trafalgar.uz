e23411d7c85f1759f0f7a95fc14b6f8fe62a5ece
erkinovasrorbek001 <erkinovasrorbek3@gmail.com>
2025-07-30 23:39:29 +0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <p className='text-5xl text-red-400 uppercase'>Hello world</p>
+    </>
+  )
+}
+
+export default App
